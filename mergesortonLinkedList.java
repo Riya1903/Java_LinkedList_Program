0a51e392df44e71f32d4d7a88d0feec2c944b056
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-06-20 22:38:09 +0530

    Add files via upload

---
+++ mergesortonLinkedList.java
@@ -0,0 +1,119 @@
+public class mergesortonLinkedList {
+    public static class Node
+    {
+        int data;
+        Node next;
+        public Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+        public static Node head;
+        public static Node tail;
+        public void addfirst(int data)
+        { 
+            //step 1-create a newnode
+            Node newnode=new Node(data);
+            if(head==null)
+            {
+                head=tail=newnode;
+                return;
+            }
+            //step 2-newnode next=head
+            newnode.next=head;
+            //step 3-head=newnode
+            head=newnode;
+    
+        }
+public Node mergesort(Node head)
+{
+    if(head==null && head.next!=null)
+    {
+        return head;
+    }
+    //find mid
+    Node mid=getMid(head);
+    
+    //left &right MS
+    Node righthead=mid.next;
+    mid.next=null;
+    Node newleft=mergesort(head);
+    Node newright=mergesort(righthead);
+
+    //merge
+    return merge(newleft,newright);
+}
+private Node getMid(Node head)
+{
+    Node slow=head;
+    Node fast=head.next;
+    while(fast!=null &&fast.next!=null)
+    {
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+  return slow;
+}
+private Node merge(Node head1,Node head2)
+{
+    Node mergedLL=new Node(-1);
+    Node temp=mergedLL;
+    while(head1!=null || head2!=null)
+    {
+        if(head1.data<=head2.data)
+        {
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        else{
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+    }
+    while(head1!=null)
+    {
+        temp.next=head1;
+        head1=head1.next;
+        temp=temp.next;
+    }
+    while(head2!=null)
+    {
+        temp.next=head2;
+        head2=head2.next;
+        temp=temp.next;
+    }
+    return mergedLL.next;
+}
+public void print()
+{
+            Node temp=head;
+            if(head==null)
+            {
+                System.out.println("linkedList is empty");
+                return;
+            }
+            while(temp!=null)
+            {
+                System.out.print(temp.data+"->");
+                temp=temp.next;
+            }
+            System.out.println("null");
+}
+
+public static void main(String[] args) {
+     mergesortonLinkedList ll=new mergesortonLinkedList();
+     ll.addfirst(1);
+     ll.addfirst(2);
+     ll.addfirst(3);
+     ll.addfirst(4);
+     ll.addfirst(5);
+     //5->4->3->2->1
+     ll.print();
+     ll.head=ll.mergesort(ll.head);
+     ll.print();
+}
+
+}
